829a235062f789304d041cc56765ad0246847bd6
juan <jbonifacio@untref.edu.ar>
2018-08-14 16:30:20 -0300

    agregado capturados de amigos por referencia

---
+++ captureFriendsByreferencia.js
@@ -0,0 +1,24 @@
+var refreshIntervalId = setInterval(function(){
+
+andabien();
+andabien();
+
+
+
+
+setTimeout(function(){ catcher();
+
+
+setTimeout(function(){ ajax(); }, 5000);
+
+
+ }, 2000);
+
+
+}, 20000);
+
+
+clearInterval(refreshIntervalId);
+
+
+
